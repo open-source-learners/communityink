789b3e1b32cf8ec39a2e82eece60e52c0d35d1f0
Arman Grema <abdurrahmangrema19@gmail.com>
2022-10-09 09:08:54 -1100

    user identification routes are defined

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express()
 
+// app.use({extends})
+
+
 app.get('/', (req,res)=>{
     res.send("<h1>Welcome to studoor</h1>")
 })
+
+app.use('/auths', require('./routes/auths'));
 
 app.listen(4020, console.log('server is running on port 4020'));
---
+++ packages/backend/routes/auths.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.post('/login',(req,res)=>{
+    res.json({msg:"Dear user you hit the login route"})
+});
+
+router.post('/register',(req,res)=>{
+    res.json({msg:"Dear user you hit the registration route"})
+})
+
+router.post('/password/reset',(req,res)=>{
+    res.json({msg:"Dear user you hit the forgot password route"})
+})
+
+module.exports = router;
